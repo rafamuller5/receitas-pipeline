98f9c3d7f2ccc7331fd44eb7002a1e6aa4dee0ba
rafamuller5 <rafaellamuller5@gmail.com>
2026-06-20 14:02:50 -0300

    teste

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login — Sistema de Receitas</title>
+    <title>Login — Sistema de Receitaz</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
